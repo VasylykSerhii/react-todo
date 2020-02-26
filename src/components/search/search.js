481d68185f0a4a0f95639e7344610db4e39d6032
Serhii <vasylyk29@gmail.com>
2020-02-26 10:22:25 +0200

    add css

---
+++ src/components/search/search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './search.css';
+
+const Search = () => {
+  return (
+    <input type="text"
+      className="form-control search-input"
+      placeholder="type to search" />
+  );
+};
+
+export default Search
