8514f946052707ae9010e745abb14ca51e445baf
Serhii <vasylyk29@gmail.com>
2020-02-26 17:41:07 +0200

    Updates

---
+++ precache-manifest.412998fe41d5b35374a769a3a417e1ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d29991e9e614e610fb8a1e1fd584ec4",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "1e4c92c2957fa1fb626f",
+    "url": "/react-todo/static/css/main.08a4aa57.chunk.css"
+  },
+  {
+    "revision": "c48bbdf2c3e77a1eb98d",
+    "url": "/react-todo/static/js/2.1df2129d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-todo/static/js/2.1df2129d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e4c92c2957fa1fb626f",
+    "url": "/react-todo/static/js/main.727a3cf9.chunk.js"
+  },
+  {
+    "revision": "bc3adbac834a5b3e95f4",
+    "url": "/react-todo/static/js/runtime-main.2e76b83e.js"
+  }
+]);
